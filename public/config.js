b53cb62a79e2d764a270ed459d0a211501308e95
Moustapha Sec <admin@hopital-thies.sn>
2026-07-19 11:38:01 +0000

    feat(saas): fix env extraction and html patching

--- public/config.js
+++ public/config.js
@@ -1,6 +1,7 @@
 window.ClinicConfig = {
     name: "Hôpital Régional",
     subtitle: "El Hadji Ahmadou Sakhir Ndiéguène",
+    fullName: "Hôpital Régional<br><span>El Hadji Ahmadou Sakhir Ndiéguène</span>",
     logo: "https://res.cloudinary.com/dzcnlr1yz/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1742394317/f1oy2jfpkujbopbqlrd2.png",
     primaryColor: "#2563EB",
     primaryDarkColor: "#1E40AF",
